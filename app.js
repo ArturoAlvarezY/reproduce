9e2f86b036d9a620fc469c7eb237a725db5d8649
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-06-19 18:41:54 +0200

    adding the emojis for each yellow square with Javascript!

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-///yellow, blue, red, green 🧸​🐻​👶🏽​
+///yellow, blue, red, green 
 
 const yellowBox= document.querySelectorAll('.yellowBox');
 const greenBox= document.querySelectorAll('.greenBox');
 const blueBox= document.querySelectorAll('.blueBox');
 const redBox= document.querySelectorAll('.redBox');
 
-yellowBox.forEach(element => {
-    element.addEventListener('mouseover', ()=>{
-       element.textContent= '💜​​'
-    })
-});
+let yellowEmojis= ['👶🏽','💜','🧸'];
 
+
+if(yellowBox.length=== yellowEmojis.length){
+
+    yellowBox.forEach((element, index) => {
+
+        element.addEventListener('mouseover', ()=>{
+                element.textContent= yellowEmojis[index];
+            })
+        })
+    };
+
+
+
+////Puedo hacer un array con los íconos con un index ++ conforme vaya pasando el mouse por encima
